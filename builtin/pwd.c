6e568d77c1373c27317ad8f7cbdc920a68a52ba3
gemstoneyang <bennyws99@gmail.com>
2023-01-08 16:22:20 +0900

    [refact] change pwd return type

--- builtin/pwd.c
+++ builtin/pwd.c
@@ -6,24 +6,25 @@
 /*   By: wonyang <wonyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 21:24:18 by wonyang           #+#    #+#             */
-/*   Updated: 2023/01/07 12:40:11 by wonyang          ###   ########seoul.kr  */
+/*   Updated: 2023/01/08 16:07:50 by wonyang          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include "../includes/return.h"
 #include "../libft/libft.h"
 
-int	ft_pwd(char **argv)
+t_error	ft_pwd(char **argv)
 {
 	char	*path;
 
 	argv = (char **)argv;
 	path = getcwd(NULL, 0);
 	if (!path)
-		return (-1);
+		return (ERROR);
 	ft_putendl_fd(path, STDOUT_FILENO);
 	free(path);
 	path = NULL;
-	return (0);
+	return (SCS);
 }
