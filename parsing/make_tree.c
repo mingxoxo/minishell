a29455fbe624fc8f3bfd19c1565b6efec0194757
jeongmin <yesuel1111@naver.com>
2023-01-11 22:39:28 +0900

    [rename] cross -> dsv(Delimiter Separated Values) + [modify] norm error

--- parsing/make_tree.c
+++ parsing/make_tree.c
@@ -6,7 +6,7 @@
 /*   By: jeongmin <jeongmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:13:27 by jeongmin          #+#    #+#             */
-/*   Updated: 2023/01/11 22:33:20 by jeongmin         ###   ########.fr       */
+/*   Updated: 2023/01/11 22:39:16 by jeongmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ t_error	make_cmd(t_tnode **node, t_list **lst)
 {
 	t_error	errno;
 
-	if (!node || is_root_symbol(node->content))
+	if (!(*node) || is_root_symbol((*node)->content))
 	{
-		if (make_new_node(&node) == ERROR)
+		if (make_new_node(node) == ERROR)
 			return (ERROR);
 	}
 	if (is_this_symbol((*lst)->content, T_IO))
-		errno = make_t_io(node, lst);
+		errno = make_t_io((*node), lst);
 	else if (is_this_symbol((*lst)->content, T_WORD))
-		errno = make_t_word(node, (*lst));
+		errno = make_t_word((*node), (*lst));
 	return (errno);
 }
 
@@ -42,8 +42,8 @@ t_tnode	*make_tree(t_list *lst)
 	{
 		if (is_this_symbol(tmp->content, T_PAREN))
 			errno = make_t_paren(&node, &tmp);
-		else if (is_root_symbol(tmp->content))
-			errno = make_cross_node(&node, tmp);
+		else if (is_dsv_symbol(tmp->content))
+			errno = make_dsv_node(&node, tmp);
 		else
 			errno = make_cmd(&node, tmp);
 		if (errno == ERROR)
