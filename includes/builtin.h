a8ff73197baf4b4dddb9b54f1e07a60add518ef5
gemstoneyang <bennyws99@gmail.com>
2023-01-13 21:52:16 +0900

    [refact] builtin/ : change functions return value

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: wonyang <wonyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 20:33:04 by wonyang           #+#    #+#             */
-/*   Updated: 2023/01/13 20:18:01 by wonyang          ###   ########seoul.kr  */
+/*   Updated: 2023/01/13 21:51:17 by wonyang          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 # include "return.h"
 # include "ds_envp.h"
 
-int		ft_echo(char **argv);
-t_error	ft_pwd(char **argv);
+int	ft_echo(char **argv);
+int	ft_pwd(char **argv);
 int	ft_cd(char **argv, t_envp *envp);
-int		ft_env(char **argv, t_envp *envp);
-t_error	ft_export(char **argv, t_envp *envp);
-t_error	ft_unset(char **argv, t_envp *envp);
-int		ft_exit(char **argv);
+int	ft_env(char **argv, t_envp *envp);
+int	ft_export(char **argv, t_envp *envp);
+int	ft_unset(char **argv, t_envp *envp);
+int	ft_exit(char **argv);
 
 #endif
