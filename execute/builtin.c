aacc8a358d34cb3f6a2564272b27c0b4f5695945
gemstoneyang <bennyws99@gmail.com>
2023-01-14 13:00:30 +0900

    [refact] builtin.c : refactoring is_builtin_cmd function

--- execute/builtin.c
+++ execute/builtin.c
@@ -6,7 +6,7 @@
 /*   By: wonyang <wonyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 18:36:41 by wonyang           #+#    #+#             */
-/*   Updated: 2023/01/14 12:52:46 by wonyang          ###   ########seoul.kr  */
+/*   Updated: 2023/01/14 12:58:24 by wonyang          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,9 @@ extern t_global	g_var;
 bool	is_builtin_cmd(t_tnode *node)
 {
 	t_token	*token;
+	const char	cmd[7][7] = {"echo", "pwd", "cd", "env", "export", \
+								"unset", "exit"};
+	int		i;
 
 	if (!node || !node->content)
 	{
@@ -33,14 +36,13 @@ bool	is_builtin_cmd(t_tnode *node)
 	token = (t_token *)node->content;
 	if (token->type != T_WORD)
 		return (false);
-	if (ft_strcmp(token->str, "echo") == 0
-		|| ft_strcmp(token->str, "pwd") == 0
-		|| ft_strcmp(token->str, "cd") == 0
-		|| ft_strcmp(token->str, "env") == 0
-		|| ft_strcmp(token->str, "export") == 0
-		|| ft_strcmp(token->str, "unset") == 0
-		|| ft_strcmp(token->str, "exit") == 0)
-		return (true);
+	i = 0;
+	while (i < 7)
+	{
+		if (ft_strncmp(token->str, (char *)(cmd[i]), ft_strlen(cmd[i])) == 0)
+			return (true);
+		i++;
+	}
 	return (false);
 }
 
