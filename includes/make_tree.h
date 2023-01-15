f1f6b8130b4f5cfb447beb36d4d602219b68be2b
jeongmin <yesuel1111@naver.com>
2023-01-15 19:11:48 +0900

    [modify] 함수 정리

--- includes/make_tree.h
+++ includes/make_tree.h
@@ -6,7 +6,7 @@
 /*   By: jeongmin <jeongmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 16:01:53 by jeongmin          #+#    #+#             */
-/*   Updated: 2023/01/15 19:08:10 by jeongmin         ###   ########.fr       */
+/*   Updated: 2023/01/15 19:11:13 by jeongmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,6 @@ t_error	subst_env(t_tnode *node);
 // subst_env_*.c
 t_error	env_first_step(t_token *token);
 t_error	env_second_step(char *str, t_tnode *node);
-
-// subset_env_lst.c
 t_error	subst_env_lst(t_token *token, int *arr, t_list **head);
 
 // del_quote.c
